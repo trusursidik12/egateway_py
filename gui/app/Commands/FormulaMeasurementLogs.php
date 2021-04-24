3ca5a9095919340002a0cbb55ceeb77cbf1ce754
warih suryono <warih_suryono@yahoo.com>
2021-04-24 11:27:37 +0700

    aaa

--- gui/app/Commands/FormulaMeasurementLogs.php
+++ gui/app/Commands/FormulaMeasurementLogs.php
@@ -2,6 +2,7 @@
 
 namespace App\Commands;
 
+use App\Models\m_configuration;
 use CodeIgniter\CLI\BaseCommand;
 use CodeIgniter\CLI\CLI;
 use App\Models\m_labjack_value;
@@ -20,12 +21,16 @@ class FormulaMeasurementLogs extends BaseCommand
 	protected $parameters;
 	protected $labjack_values;
 	protected $measurement_logs;
+	protected $configurations;
+	protected $lastPutData;
 
 	public function __construct()
 	{
 		$this->parameters =  new m_parameter();
 		$this->labjack_values =  new m_labjack_value();
 		$this->measurement_logs =  new m_measurement_log();
+		$this->configurations =  new m_configuration();
+		$this->lastPutData = "0000-00-00 00:00";
 	}
 
 	/**
@@ -68,7 +73,77 @@ class FormulaMeasurementLogs extends BaseCommand
 	 *
 	 * @param array $params
 	 */
+
+	public function get_measurement_logs_range($minute)
+	{
+		$id_end = @$this->measurement_logs->orderBy("xtimestamp DESC")->findAll()[0]->id;
+		$lasttime = date("Y-m-d H:i:%", mktime(date("H"), date("i") - $minute));
+		$mm = date("i") * 1;
+		$current_time = date("Y-m-d H:i");
+		if ($mm % $minute == 0 && $this->lastPutData != $current_time) {
+			$id_start = @$this->measurement_logs->where("xtimestamp >= '" . $lasttime . ":00'")->where("is_averaged", 0)->orderBy("xtimestamp")->findAll()[0]->id;
+			if ($id_start > 0) {
+				$measurement_logs = $this->measurement_logs->where("id BETWEEN '" . $id_start . "' AND '" . $id_end . "'")->where("is_averaged", 0)->findAll();
+				$return["id_start"] = $id_start;
+				$return["id_end"] = $id_end;
+				$return["waktu"] = $current_time . ":00";
+				$return["data"] = $measurement_logs;
+				return $return;
+			} else {
+				return 0;
+			}
+		} else {
+			return 0;
+		}
+	}
+
+	public function measurements_averaging()
+	{
+		$configuration = $this->configurations->where("id", 1)->findAll()[0];
+		$measurement_logs = $this->get_measurement_logs_range($configuration->interval_average);
+		if ($measurement_logs != 0) {
+			foreach ($measurement_logs["data"] as $measurement_log) {
+				@$total[$measurement_log->parameter_id] += $measurement_log->value;
+				@$numdata[$measurement_log->parameter_id]++;
+			}
+			foreach ($this->parameters->findAll() as $parameter) {
+				if (@$numdata[$parameter->id] > 0) {
+					$measurements = [
+						"instrument_id" => $instrument_id,
+						"time_group" => $measurement_logs["waktu"],
+						"measured_at" => $measurement_logs["waktu"],
+						"parameter_id" => $parameter->id,
+						"value" => $total[$parameter->id] / $numdata[$parameter->id],
+						"unit_id" => $parameter->unit_id,
+						"created_at" => date("Y-m-d H:i:s"),
+						"created_by" => $this->session->get("username"),
+						"created_ip" => $_SERVER["REMOTE_ADDR"],
+						"updated_at" => date("Y-m-d H:i:s"),
+						"updated_by" => $this->session->get("username"),
+						"updated_ip" => $_SERVER["REMOTE_ADDR"],
+					];
+					if ($this->measurements->save($measurements)) {
+						// $this->sensors_m->insert_aqm_data($aqm_data_values, $aqm_data_ranges["id_start"], $aqm_data_ranges["id_end"]);
+						// $this->db->where("id BETWEEN '" . $id_start . "' AND '" . $id_end . "'");
+						// $this->db->update('aqm_data_log', ["is_sent" => "1", "sent_at" => date("Y-m-d H:i:s")]);
+
+						// $this->measurement_logs->update()
+
+						// $this->db->insert('aqm_data', $values);
+					}
+				}
+			}
+			$this->lastPutData = date("Y-m-d H:i");
+		}
+		print_r($measurement_logs);
+	}
+
 	public function run(array $params)
+	{
+		$this->measurements_averaging();
+	}
+
+	public function Xrun(array $params)
 	{
 		foreach ($this->labjack_values->findAll() as $labjack_value) {
 			$labjack[$labjack_value->labjack_id][$labjack_value->ain_id] = $labjack_value->data;
@@ -85,7 +160,5 @@ class FormulaMeasurementLogs extends BaseCommand
 			];
 			$this->measurement_logs->save($measurement_logs);
 		}
-
-		// echo json_encode($data);
 	}
 }
