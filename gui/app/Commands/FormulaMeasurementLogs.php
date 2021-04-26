e3a91471ecd590422de14fc9f356215b9ab690b2
warih suryono <warih_suryono@yahoo.com>
2021-04-27 06:59:01 +0700

    aaa

--- gui/app/Commands/FormulaMeasurementLogs.php
+++ gui/app/Commands/FormulaMeasurementLogs.php
@@ -6,6 +6,7 @@ use App\Models\m_configuration;
 use CodeIgniter\CLI\BaseCommand;
 use CodeIgniter\CLI\CLI;
 use App\Models\m_labjack_value;
+use App\Models\m_measurement;
 use App\Models\m_measurement_log;
 use App\Models\m_parameter;
 
@@ -30,6 +31,7 @@ class FormulaMeasurementLogs extends BaseCommand
 		$this->labjack_values =  new m_labjack_value();
 		$this->measurement_logs =  new m_measurement_log();
 		$this->configurations =  new m_configuration();
+		$this->measurements =  new m_measurement();
 		$this->lastPutData = "0000-00-00 00:00";
 	}
 
@@ -76,12 +78,12 @@ class FormulaMeasurementLogs extends BaseCommand
 
 	public function get_measurement_logs_range($minute)
 	{
-		$id_end = @$this->measurement_logs->orderBy("xtimestamp DESC")->findAll()[0]->id;
+		$id_end = @$this->measurement_logs->orderBy("id DESC")->findAll()[0]->id;
 		$lasttime = date("Y-m-d H:i:%", mktime(date("H"), date("i") - $minute));
 		$mm = date("i") * 1;
 		$current_time = date("Y-m-d H:i");
 		if ($mm % $minute == 0 && $this->lastPutData != $current_time) {
-			$id_start = @$this->measurement_logs->where("xtimestamp >= '" . $lasttime . ":00'")->where("is_averaged", 0)->orderBy("xtimestamp")->findAll()[0]->id;
+			$id_start = @$this->measurement_logs->where("xtimestamp >= '" . $lasttime . ":00'")->where("is_averaged", 0)->orderBy("id")->findAll()[0]->id;
 			if ($id_start > 0) {
 				$measurement_logs = $this->measurement_logs->where("id BETWEEN '" . $id_start . "' AND '" . $id_end . "'")->where("is_averaged", 0)->findAll();
 				$return["id_start"] = $id_start;
@@ -103,44 +105,32 @@ class FormulaMeasurementLogs extends BaseCommand
 		$measurement_logs = $this->get_measurement_logs_range($configuration->interval_average);
 		if ($measurement_logs != 0) {
 			foreach ($measurement_logs["data"] as $measurement_log) {
+				@$instrument_id[$measurement_log->parameter_id] = $measurement_log->instrument_id;
 				@$total[$measurement_log->parameter_id] += $measurement_log->value;
 				@$numdata[$measurement_log->parameter_id]++;
 			}
 			foreach ($this->parameters->findAll() as $parameter) {
 				if (@$numdata[$parameter->id] > 0) {
 					$measurements = [
-						"instrument_id" => $instrument_id,
+						"instrument_id" => $instrument_id[$parameter->id],
 						"time_group" => $measurement_logs["waktu"],
 						"measured_at" => $measurement_logs["waktu"],
 						"parameter_id" => $parameter->id,
 						"value" => $total[$parameter->id] / $numdata[$parameter->id],
 						"unit_id" => $parameter->unit_id,
 						"created_at" => date("Y-m-d H:i:s"),
-						"created_by" => $this->session->get("username"),
-						"created_ip" => $_SERVER["REMOTE_ADDR"],
+						"created_by" => "",
+						"created_ip" => "127.0.0.1",
 						"updated_at" => date("Y-m-d H:i:s"),
-						"updated_by" => $this->session->get("username"),
-						"updated_ip" => $_SERVER["REMOTE_ADDR"],
+						"updated_by" => "",
+						"updated_ip" => "127.0.0.1",
 					];
-					if ($this->measurements->save($measurements)) {
-						// $this->sensors_m->insert_aqm_data($aqm_data_values, $aqm_data_ranges["id_start"], $aqm_data_ranges["id_end"]);
-						// $this->db->where("id BETWEEN '" . $id_start . "' AND '" . $id_end . "'");
-						// $this->db->update('aqm_data_log', ["is_sent" => "1", "sent_at" => date("Y-m-d H:i:s")]);
-
-						// $this->measurement_logs->update()
-
-						// $this->db->insert('aqm_data', $values);
-					}
+					$this->measurements->save($measurements);
 				}
 			}
+			$this->measurement_logs->set(["is_averaged" => 1])->where("id BETWEEN '" . $measurement_logs["id_start"] . "' AND '" . $measurement_logs["id_end"] . "'")->update();
 			$this->lastPutData = date("Y-m-d H:i");
 		}
-		print_r($measurement_logs);
-	}
-
-	public function Xrun(array $params)
-	{
-		$this->measurements_averaging();
 	}
 
 	public function run(array $params)
@@ -163,5 +153,6 @@ class FormulaMeasurementLogs extends BaseCommand
 			];
 			$this->measurement_logs->save($measurement_logs);
 		}
+		$this->measurements_averaging();
 	}
 }
