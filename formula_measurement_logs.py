9f50e8a121958486e4d1448645b48d8067877d92
warih suryono <warih_suryono@yahoo.com>
2021-04-28 14:03:18 +0700

    aaa

--- formula_measurement_logs.py
+++ formula_measurement_logs.py
@@ -6,7 +6,8 @@ counter = 0
 while True:
     counter = counter + 1
     subprocess.Popen("php gui\spark command:formula_measurement_logs", shell=False)
-    if(counter >= 5):
+    if(counter >= 60):
         subprocess.Popen("php gui\spark command:sentdata", shell=False)
+        subprocess.Popen("php gui\spark command:measurement_averaging", shell=False)
         counter = 0
-    time.sleep(60)
+    time.sleep(1)
