ffa0b4ef51089369b787b28a513b38e5e0ee0e4b
warih suryono <warih_suryono@yahoo.com>
2021-04-28 09:07:22 +0700

    aaa

--- formula_measurement_logs.py
+++ formula_measurement_logs.py
@@ -3,4 +3,5 @@ import time
 
 while True:
     subprocess.Popen("php gui\spark command:formula_measurement_logs", shell=False)
+    subprocess.Popen("php gui\spark command:sentdata", shell=False)
     time.sleep(1)
